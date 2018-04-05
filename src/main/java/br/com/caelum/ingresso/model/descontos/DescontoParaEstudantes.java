6075bea6882b37022ab6972fa9cdfa9d95037641
Lilian <lilian@gmail.com>
2018-04-05 12:56:10 -0300

    inclusao da compra de ingressos

---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantes.java
@@ -0,0 +1,13 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+
+public class DescontoParaEstudantes implements Desconto{
+
+	@Override
+	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal) {
+		// TODO Auto-generated method stub
+		return precoOriginal.divide(new BigDecimal(2));
+	}
+
+}
